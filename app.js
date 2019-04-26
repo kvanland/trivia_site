07921ac33817c47e0e5ca52ac01e607c511f19a0
Kyle Van Landingham <kylevanlandingham9@gmail.com>
2019-04-25 23:15:18 -0700

    Reseting game

--- app.js
+++ app.js
@@ -3,26 +3,43 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+// Game Logic
 var host = '';
 var players = {};
 var buzzers = {};
 var buzzerPlace = 0;
+var questions = [];
+var questionsPlace = 0;
+
+// Allows the server to serve static files such as index.js
 app.use(express.static(__dirname));
 
-
+// Server setup
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
+
     socket.on('host', function (name) {
         if (host === '') {
             host = name;
+            fetchQuestions();
             socket.emit('host', true);
+            const request = require('request');
+            request('http://jservice.io/api/random?count=100', function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    questions = JSON.parse(body);
+                    processQuestions();
+                    socket.emit('nextQuestion', questions[questionsPlace++]);
+                }
+            });
+
         } else {
             socket.emit('host', false);
         }
     });
+
     socket.on('join', function (name) {
         if (host === '' || name in players) {
             socket.emit('join', false);
@@ -45,17 +62,62 @@ io.on('connection', function (socket) {
     });
 
     socket.on('buzz-in', function (name) {
-        if(!(name in buzzers)) {
+        if (!(name in buzzers)) {
             buzzers[name] = ++buzzerPlace;
             io.emit('updateBuzzers', buzzers);
+
         }
     });
 
-    socket.on('resetBuzzIn', function(){
+    socket.on('resetBuzzIn', function () {
         buzzers = {};
-        io.emit('updateBuzzers', buzzers);
+        buzzerPlace = 0;
+        io.emit('clearBuzzers');
+    });
+
+    socket.on('nextQuestion', function () {
+        socket.emit('nextQuestion', questions[questionsPlace++]);
+        if (questionsPlace == 100) {
+            quesionsPlace = 0;
+            fetchQuestions();
+        }
+    });
+
+    socket.on('deleteGame', function () {
+        resetGame()
+        io.emit('gameDeleted');
     });
 });
+
+function fetchQuestions() {
+    const request = require('request');
+    request('http://jservice.io/api/random?count=100', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            questions = JSON.parse(body);
+            processQuestions();
+        }
+    });
+}
+
+function processQuestions() {
+    processedQuestions = [];
+    questions.forEach(function (q) {
+        var question = q['category']['title'] + ': ' + q['question'];
+        var answer = q['answer'];
+        processedQuestions.push({ q: question, a: answer });
+    });
+    questions = processedQuestions;
+}
+
+function resetGame() {
+    host = '';
+    players = {};
+    buzzers = {};
+    buzzerPlace = 0;
+    questions = [];
+    questionsPlace = 0;
+}
+
 
 http.listen(3000, function () {
     console.log('listening on 3000');
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-hosting = false;
-playing = false;
-rows = [];
+var hosting = false;
+var playing = false;
+var rows = [];
 
 $(function () {
     $('.alert').alert()
@@ -30,7 +30,7 @@ $(function () {
     });
 
     socket.on('newPlayer', function (name) {
-        if(name === $('#username').html().substr(1) || !playing){
+        if (name === $('#username').html().substr(1) || !playing) {
             return;
         }
         if (hosting) {
@@ -56,9 +56,25 @@ $(function () {
         rows.forEach(function (row) {
             var place = $(row).find('.buzz-place');
             var name = $(row).find('.username-row').text();
-            console.log($(buzzers));
             $(place).html(buzzers[name]);
         });
+    });
+
+    socket.on('nextQuestion', function (question) {
+        if (hosting) {
+            $('#current-question-host').html(question['q']);
+            $('#current-answer-host').html(question['a']);
+        }
+    });
+
+    socket.on('clearBuzzers', function () {
+        rows.forEach(function (row) {
+            $(row).find('.buzz-place').html(0);
+        });
+    });
+
+    socket.on('gameDeleted', function (name) {
+        resetGame();
     });
 
     $('#host-btn').click(function (e) {
@@ -70,6 +86,7 @@ $(function () {
         }
         return false;
     });
+
     $('#join-btn').click(function (e) {
         let username = $('#username-input').val().trim();
         if (username === '') {
@@ -78,6 +95,10 @@ $(function () {
             socket.emit('join', username);
         }
         return false;
+    });
+
+    $('#next-btn').click(function (e) {
+        socket.emit('nextQuestion');
     });
 
     $('#buzz-btn').click(function (e) {
@@ -90,11 +111,16 @@ $(function () {
         socket.emit('resetBuzzIn');
     });
 
+    $('#end-game-btn').click(function(e){
+        socket.emit('deleteGame');
+    });
+
     $('body').on('click', '.plus-btn', function () {
         var username = $(this).closest('tr').find('.username-row').text();
         socket.emit('incrementScore', username);
         return false;
     });
+
     $('body').on('click', '.minus-btn', function () {
         var username = $(this).closest('tr').find('.username-row').text();
         socket.emit('decrementScore', username);
@@ -104,14 +130,13 @@ $(function () {
 
 function initialize_join(players) {
     $('#username').text('@' + $('#username-input').val());
-    var table = hosting ? $('#host-table > tbody:last-child') : ('#player-table > tbody:last-child');
-    console.log($(players));
-    for(var player in players){
-        if(hosting){
+    var table = hosting ? $('#host-table > tbody') : ('#player-table > tbody');
+    for (var player in players) {
+        if (hosting) {
             var row = $('<tr><th class="username-row" scope="row">' + player + '</th><td class="score">' + players[player] + '</td><td class="buzz-place">0</td><td><button class="btn btn-primary plus-btn" type="button">+1</button><button class="btn btn-danger minus-btn" type="button">-1</button></td></tr>');
             rows.push(row);
             $(table).append(row)
-        } else{
+        } else {
             var row = $('<tr><th class="username-row" scope="row">' + player + '</th><td class="score">' + players[player] + '</td><td class="buzz-place">0</td></tr>');
             rows.push(row);
             $(table).append(row);
@@ -121,6 +146,24 @@ function initialize_join(players) {
 
 function initialize_host() {
 
+}
+
+function resetGame() {
+    if(hosting){
+        $('#host-container').addClass('d-none');
+        $('#init-container').removeClass('d-none');
+    } else{
+        $('#join-container').addClass('d-none');
+        $('#init-container').removeClass('d-none');
+    }
+    hosting = false;
+    playing = false;
+    rows = [];
+    $('#host-table > tbody').html("");
+    $('#player-table > tbody').html("");
+    $('#current-question-host').html("");
+    $('#current-answer-host').html("");
+    $('#current-question').html("");
 }
 
 /*
