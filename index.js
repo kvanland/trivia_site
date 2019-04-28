42a23b542378fbd500d8b2b3df449034aa995da6
Kyle Van Landingham <kylevanlandingham9@gmail.com>
2019-04-28 16:45:15 -0700

    Fixed some formatting as well as included former global variables in encapsulating function on client side

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-var hosting = false;
-var playing = false;
-var rows = [];
-var name = '';
-
-let priorities = {
-    'PRIMARY': 0,
-    'SECONDARY': 1,
-    'SUCCESS': 2,
-    'DANGER': 3,
-    'WARNING': 4,
-    'INFO': 5,
-    'LIGHT': 6,
-    'DARK': 7
-
-}
-
 $(function () {
-    $('.alert').alert()
     var socket = io();
+    var hosting = false;
+    var playing = false;
+    var rows = [];
+    var name = '';
 
+    let priorities = {
+        'PRIMARY': 0,
+        'SECONDARY': 1,
+        'SUCCESS': 2,
+        'DANGER': 3,
+        'WARNING': 4,
+        'INFO': 5,
+        'LIGHT': 6,
+        'DARK': 7
+    }
+
+    /**
+     * Function in response to a request to host a game. Will either permit hosting and set up necessary view and data or deny and notify user.
+     * @param {boolean} allowed - A boolean sent by the server indicating permission to host a game
+     */
     socket.on('host', function (allowed) {
         if (allowed) {
             $('#host-container').removeClass('d-none');
@@ -27,10 +28,15 @@ $(function () {
             playing = true;
             show_alert('Successfully created the game!', 2500, $('#host-alert'), priorities.SUCCESS);
         } else {
-            show_alert('There is currently another host. Try again later or join the current game.', 5000, $('#login-warning'), priorities.WARNING);
+            show_alert('There is currently another host. Try again later or join the current game.', 5000, $('#login-alert'), priorities.WARNING);
         }
-
     });
+
+    /**
+     * Function in response to a request to join a game. Will either permit joining and set up necessary view and data or deny and notify user.
+     * @param {object} players - Object containing information of players currently in the game the user is attempting to join
+     * @param {boolean} allowed - A boolean sent by the sever indicating permission to join a game
+     */
     socket.on('join', function (players, allowed) {
         if (allowed) {
             $('#join-container').removeClass('d-none');
@@ -43,56 +49,80 @@ $(function () {
         }
     });
 
-    socket.on('newPlayer', function (name) {
-        if (name === $('#username').html().substr(1) || !playing) {
+    /**
+     * Function in response to a new player joining the game. Will update game view to reflect new player and notify with an alert that the player has joined.
+     * @param {string} username - The name of the user who joined the game
+     */
+    socket.on('newPlayer', function (username) {
+        if (username === $('#username').html().substr(1) || !playing) {
             return;
         }
         if (hosting) {
-            var row = $('<tr><th class="username-row" scope="row">' + name + '</th><td class="score">0</td><td class="buzz-place">0</td><td><button class="btn btn-primary plus-btn" type="button">+1</button><button class="btn btn-danger minus-btn" type="button">-1</button></td></tr>');
+            var row = $('<tr><th class="username-row" scope="row">' + username + '</th><td class="score">0</td><td class="buzz-place"></td><td><button class="btn btn-primary plus-btn" type="button">+1</button><button class="btn btn-danger minus-btn" type="button">-1</button></td></tr>');
             rows.push(row);
             $('#host-table > tbody:last-child').append(row);
-            show_alert(name + ' joined the game!', 2500, $('#host-alert'), priorities.INFO);
+            show_alert(username + ' joined the game!', 2500, $('#host-alert'), priorities.INFO);
         } else {
-            var row = $('<tr><th class="username-row" scope="row">' + name + '</th><td class="score">0</td><td class="buzz-place">0</td></tr>');
+            var row = $('<tr><th class="username-row" scope="row">' + username + '</th><td class="score">0</td><td class="buzz-place"></td></tr>');
             rows.push(row);
             $('#player-table > tbody:last-child').append(row);
-            show_alert(name + ' joined the game!', 2500, $('#join-alert'), priorities.INFO);
+            show_alert(username + ' joined the game!', 2500, $('#join-alert'), priorities.INFO);
         }
     });
 
+    /**
+     * Function in response to the server updating a player's scores. Will update game view to reflect change in scores.
+     * @param {string} username - Name of player whose score was updated
+     * @param {number} score - New score value
+     * @param {boolean} increased - Boolean to indicate whether the score was increased or decreased
+     */
     socket.on('changeScore', function (username, score, increased) {
         rows.forEach(function (row) {
             if ($(row).find('.username-row').text() === username) {
                 $(row).find('.score').html(score);
             }
         });
-        if(username == name){
+        if (username == name) {
             $('#score').html('Score: ' + score);
         }
-        if(!hosting){
-            if(increased){
+        if (!hosting) {
+            if (increased) {
                 show_alert(username + ' got a point.', 3000, $('#join-alert'), priorities.INFO);
-            } else{
+            } else {
                 show_alert(username + ' lost a point.', 3000, $('#join-alert'), priorities.INFO);
             }
         }
-        
+
     });
 
+    /**
+     * Function in response to a player buzzing in to answer a question. Will update game view to reflect updated buzz in ordering as well as notify the host that someone buzzed in.
+     * @param {object} buzzers - object containing a mapping of player usernames who have buzzed in to their place in the buzz in ordering
+     */
     socket.on('updateBuzzers', function (buzzers) {
         rows.forEach(function (row) {
-            var place = $(row).find('.buzz-place');
             var username = $(row).find('.username-row').text();
-            $(place).html(buzzers[username]);
+            if (username in buzzers) {
+                var place = $(row).find('.buzz-place');
+                $(place).html(buzzers[username]);
+            }
         });
+        if (hosting) {
+            show_alert('BUZZ', 1000, $('#host-alert'), priorities.DANGER);
+        }
     });
 
+    /**
+     * Function in response to the server sending in a new question. Will display new question and answer to host and reset questiona and answer views for players.
+     * @param {object} question - object that contains the question and corresponding answer
+     */
     socket.on('nextQuestion', function (question) {
         if (hosting) {
             $('#current-question-host').html(question['q']);
             $('#current-answer-host').html(question['a']);
         } else {
             $('#current-question').html('Hidden by Host');
+            $('#current-answer').html('Hidden by Host');
         }
     });
 
@@ -116,7 +146,7 @@ $(function () {
 
     socket.on('clearBuzzers', function () {
         rows.forEach(function (row) {
-            $(row).find('.buzz-place').html(0);
+            $(row).find('.buzz-place').html('');
         });
         show_alert('Buzzers have been cleared', 2500, $('#host-alert'), priorities.SUCCESS);
         show_alert('Buzzers have been cleared', 2500, $('#join-alert'), priorities.INFO);
@@ -139,7 +169,7 @@ $(function () {
                 if ($(row).find('.username-row').text() === username) {
                     $(row).remove();
                     rows.splice(i, 1);
-                    if(hosting){
+                    if (hosting) {
                         show_alert(username + ' has left the game', 3000, $('#host-alert'), priorities.DANGER);
                     } else {
                         show_alert(username + ' has left the game', 3000, $('#join-alert'), priorities.DANGER);
@@ -154,9 +184,9 @@ $(function () {
         name = $('#username-input').val().trim();
         if (name === '') {
             show_alert('Invalid username', 5000, $('#login-alert'), priorities.WARNING);
-        } else if(name.length > 14){
+        } else if (name.length > 14) {
             show_alert('Username must be less than 14 characters long', 5000, $('#login-alert'), priorities.WARNING);
-        }else {
+        } else {
             name = $('#username-input').val();
             socket.emit('host', name);
         }
@@ -167,7 +197,7 @@ $(function () {
         name = $('#username-input').val().trim();
         if (name === '') {
             show_alert('Invalid username', 5000, $('#login-alert'), priorities.WARNING);
-        } else if(name.length > 14){
+        } else if (name.length > 14) {
             show_alert('Username must be less than 14 characters long', 5000, $('#login-alert'), priorities.WARNING);
         } else {
             socket.emit('join', name);
@@ -183,7 +213,7 @@ $(function () {
         socket.emit('revealQuestion');
     })
 
-    $('#reveal-answer-btn').click(function (e){
+    $('#reveal-answer-btn').click(function (e) {
         socket.emit('revealAnswer');
     })
 
@@ -217,88 +247,94 @@ $(function () {
         socket.emit('decrementScore', username);
         return false;
     });
-});
 
-function initialize_join(players) {
-    $('#username').text('@' + $('#username-input').val());
-    var table = hosting ? $('#host-table > tbody') : ('#player-table > tbody');
-    for (var player in players) {
-        if (hosting) {
-            var row = $('<tr><th class="username-row" scope="row">' + player + '</th><td class="score">' + players[player] + '</td><td class="buzz-place">0</td><td><button class="btn btn-primary plus-btn" type="button">+1</button><button class="btn btn-danger minus-btn" type="button">-1</button></td></tr>');
-            rows.push(row);
-            $(table).append(row)
-        } else {
-            var row = $('<tr><th class="username-row" scope="row">' + player + '</th><td class="score">' + players[player] + '</td><td class="buzz-place">0</td></tr>');
-            rows.push(row);
-            $(table).append(row);
+    /**
+    * Initializes the player's view of the game they join including other player information
+    * @param {object} players 
+    */
+    function initialize_join(players) {
+        $('#username').text('@' + $('#username-input').val());
+        var table = hosting ? $('#host-table > tbody') : ('#player-table > tbody');
+        for (var player in players) {
+            if (hosting) {
+                var row = $('<tr><th class="username-row" scope="row">' + player + '</th><td class="score">' + players[player] + '</td><td class="buzz-place">0</td><td><button class="btn btn-primary plus-btn" type="button">+1</button><button class="btn btn-danger minus-btn" type="button">-1</button></td></tr>');
+                rows.push(row);
+                $(table).append(row)
+            } else {
+                var row = $('<tr><th class="username-row" scope="row">' + player + '</th><td class="score">' + players[player] + '</td><td class="buzz-place">0</td></tr>');
+                rows.push(row);
+                $(table).append(row);
+            }
         }
     }
-}
 
-function resetGame() {
-    if (hosting) {
-        $('#host-container').addClass('d-none');
-        $('#init-container').removeClass('d-none');
-    } else {
-        $('#join-container').addClass('d-none');
-        $('#init-container').removeClass('d-none');
-    }
-    hosting = false;
-    playing = false;
-    rows = [];
-    name = '';
-    $('#host-table > tbody').html('');
-    $('#player-table > tbody').html('');
-    $('#current-question-host').html('');
-    $('#current-answer-host').html('');
-    $('#current-question').html('Hidden by Host');
-    
-}
 
-/*
- * This function shows an alert
- * @param message - The message to show in the alert box
- * @param time - The time before hiding the alert
- * @param element - Alert element passed into function
- * @param priorityLevel - Level of priority to assign to alert
- * @return null
- */
-function show_alert(message, time, element, priorityLevel) {
-    switch (priorityLevel) {
-        case priorities.PRIMARY:
-            $(element).html(message);
-            $(element).addClass('show alert-primary');
-            setTimeout(function () { $(element).removeClass('show alert-primary') }, time);
-            break;
-        case priorities.SECONDARY:
-            $(element).html(message);
-            $(element).addClass('show alert-secondary');
-            setTimeout(function () { $(element).removeClass('show alert-secondary') }, time);
-            break;
-        case priorities.SUCCESS:
-            $(element).html(message);
-            $(element).addClass('show alert-success');
-            setTimeout(function () { $(element).removeClass('show alert-success') }, time);
-            break;
-        case priorities.DANGER:
-            $(element).html(message);
-            $(element).addClass('show alert-danger');
-            setTimeout(function () { $(element).removeClass('show alert-danger') }, time);
-            break;
-        case priorities.WARNING:
-            $(element).html(message);
-            $(element).addClass('show alert-warning');
-            setTimeout(function () { $(element).removeClass('show alert-warning') }, time);
-            break;
-        case priorities.INFO:
-            $(element).html(message);
-            $(element).addClass('show alert-info');
-            setTimeout(function () { $(element).removeClass('show alert-info') }, time);
-            break;
-        case priorities.DARK:
-            $(element).html(message);
-            $(element).addClass('show alert-dark');
-            setTimeout(function () { $(element).removeClass('show alert-dark') }, time);
-            break;
+    /**
+     * Resets the game information client side as well as resets the application view
+     */
+    function resetGame() {
+        if (hosting) {
+            $('#host-container').addClass('d-none');
+            $('#init-container').removeClass('d-none');
+        } else {
+            $('#join-container').addClass('d-none');
+            $('#init-container').removeClass('d-none');
+        }
+        hosting = false;
+        playing = false;
+        rows = [];
+        name = '';
+        $('#host-table > tbody').html('');
+        $('#player-table > tbody').html('');
+        $('#current-question-host').html('');
+        $('#current-answer-host').html('');
+        $('#current-question').html('Hidden by Host');
+
     }
-}
+
+    /**
+     * Shows an alert for a specified amount of time
+     * @param {string} message - The message to show in the alert box
+     * @param {number} time - The time before hiding the alert
+     * @param {object} element - Alert element passed into function
+     * @param {number} priorityLevel - Level of priority to assign to alert
+     */
+    function show_alert(message, time, element, priorityLevel) {
+        switch (priorityLevel) {
+            case priorities.PRIMARY:
+                $(element).html(message);
+                $(element).addClass('show alert-primary');
+                setTimeout(function () { $(element).removeClass('show alert-primary') }, time);
+                break;
+            case priorities.SECONDARY:
+                $(element).html(message);
+                $(element).addClass('show alert-secondary');
+                setTimeout(function () { $(element).removeClass('show alert-secondary') }, time);
+                break;
+            case priorities.SUCCESS:
+                $(element).html(message);
+                $(element).addClass('show alert-success');
+                setTimeout(function () { $(element).removeClass('show alert-success') }, time);
+                break;
+            case priorities.DANGER:
+                $(element).html(message);
+                $(element).addClass('show alert-danger');
+                setTimeout(function () { $(element).removeClass('show alert-danger') }, time);
+                break;
+            case priorities.WARNING:
+                $(element).html(message);
+                $(element).addClass('show alert-warning');
+                setTimeout(function () { $(element).removeClass('show alert-warning') }, time);
+                break;
+            case priorities.INFO:
+                $(element).html(message);
+                $(element).addClass('show alert-info');
+                setTimeout(function () { $(element).removeClass('show alert-info') }, time);
+                break;
+            case priorities.DARK:
+                $(element).html(message);
+                $(element).addClass('show alert-dark');
+                setTimeout(function () { $(element).removeClass('show alert-dark') }, time);
+        }
+    }
+});
