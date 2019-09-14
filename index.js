2466d1db0ab383ad00c0aee00953ab4cf4487752
Kyle Van Landingham <kylevanlandingham9@gmail.com>
2019-09-13 18:41:58 -0700

    Fixed missing index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#host-btn').click(function (e) {
+        window.location.href = '/host.html';
+    });
+
+    $('#join-btn').click(function (e) {
+        window.location.href = '/join.html';
+    });
+
+});
